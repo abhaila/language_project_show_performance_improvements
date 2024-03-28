7503d969a62530ed6c1e368645b1223bece05244
areeb.bhaila <areeb.bhaila@gmail.com>
2024-03-28 08:38:55 +0000

    PR to share lightening talk notes

--- app/services/words/import_from_text.rb
+++ app/services/words/import_from_text.rb
@@ -4,16 +4,35 @@ module Words
   class ImportFromText
     class << self
       def call(text, language)
-        all_words = text.split(Word::REGEX)
-        all_words = all_words.reject(&:empty?)
-        all_words = all_words.map(&:downcase)
+        # 1. Benchmark my code performance and observe logs
+        # 2. Reduce database calls
+        # 2a. Reduce query count
+        # 2b. Reduce write operations
+        # 3. Create from model rather than association
+        # 4. Reduce the number of iterations
+        # 5. Transforming the data to make it a quicker process (hashes vs. arrays)
 
-        all_words.each do |word|
-          db_record = lanuguage.word.find_by(name: word)
-          next if db_record
-
-          language.words.create(name: word)
+        all_words = text.split(Word::REGEX).filter_map do |word|
+          word.empty? ? nil : word.downcase
         end
+
+        db_words = language.words.where(name: all_words).index_by(&:name)
+
+        ActiveRecord::Base.transaction do
+          all_words.each do |word|
+            db_word = db_words[word]
+            next if db_word
+
+            db_words[word] = Word.create(name: word, language: language)
+          end
+        end
+
+        # In the meet-up we alternatively discussed the following code snippet:
+        # all_words_array = all_words.uniq.map { |word| { name: word, language_id: language.id } }
+        # Word.insert_all(all_words_array)
+        # this significantly reduces the number of iterations and write operations
+        # by negotiating with the database in one call
+        # it however does skip all validations and callbacks
       end
     end
   end
